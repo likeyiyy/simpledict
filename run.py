e1e2cb1e8d9afcc1a6a8549e4a0bfad9216772ec
likeyiyy <929812468@qq.com>
2017-10-09 16:21:18 +0800

    :meger: emailhook
    Signed-off-by: likeyiyy <929812468@qq.com>

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def add():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+
+    return render_template('login.html', error=error)
+
 
